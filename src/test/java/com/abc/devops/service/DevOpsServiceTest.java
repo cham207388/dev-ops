39707e2f016753b7d00892f3d1c26b9824116fcd
Alhagie Bai Cham <cham207388@gmail.com>
2020-11-26 14:41:29 -0800

    fix null pointer

--- src/test/java/com/abc/devops/service/DevOpsServiceTest.java
+++ src/test/java/com/abc/devops/service/DevOpsServiceTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DevOpsServiceTest {
-    private DevOpsService service;
+    private DevOpsService service = new DevOpsService();
 
     @Test
     void devOps() {
